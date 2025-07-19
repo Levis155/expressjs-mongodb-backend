f303c5dd3b96ae87b2dcaab8c03c0e884f63653d
Levis155 <levismbui@gmail.com>
2025-07-19 09:33:36 +0300

    implement route for updating employee data

--- controllers/employee.controllers.js
+++ controllers/employee.controllers.js
@@ -1,36 +1,55 @@
 import Employee from "../models/employee.model.js";
 
 export const fetchAllEmployees = async (req, res) => {
-    try{
-        const allEmployees = await Employee.find();
+  try {
+    const allEmployees = await Employee.find();
 
-        res.status(200).json({data: allEmployees})
-    } catch (err) {
-        res.status(500).json({message: "Failed to fetch employees."})
-    }
-}
+    res.status(200).json({ data: allEmployees });
+  } catch (err) {
+    res.status(500).json({ message: "Failed to fetch employees." });
+  }
+};
 
 export const addSingleEmployee = async (req, res) => {
-    try{
-        const {empName, age, location, email} = req.body;
+  try {
+    const { empName, age, location, email } = req.body;
 
-        const newEmployee = await Employee.create({empName, age, location, email});
+    const newEmployee = await Employee.create({
+      empName,
+      age,
+      location,
+      email,
+    });
 
-        res.status(201).json({data: newEmployee});
-    } catch (err) {
-        res.status(500).json({message: "Failed to add employee"})
-    }
-}
+    res.status(201).json({ data: newEmployee });
+  } catch (err) {
+    res.status(500).json({ message: "Failed to add employee" });
+  }
+};
 
 export const addMultipleEmployees = async (req, res) => {
   try {
     const employees = req.body;
 
-    const createdEmployees = await Employee.create(employees); 
+    const createdEmployees = await Employee.create(employees);
 
     res.status(201).json({ data: createdEmployees });
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: "Failed to add employees" });
+  }
+};
+
+export const updateEmployee = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updatedFields = req.body;
+
+    const updatedEmployee = await Employee.findByIdAndUpdate(id, updatedFields, { new: true, runValidators: true });
+
+    res.status(200).json({ data: updatedEmployee });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Failed to update employee" });
   }
 };
--- routes/employee.routes.js
+++ routes/employee.routes.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { fetchAllEmployees, addSingleEmployee, addMultipleEmployees } from "../controllers/employee.controllers.js";
+import { fetchAllEmployees, addSingleEmployee, addMultipleEmployees, updateEmployee } from "../controllers/employee.controllers.js";
 
 const router = Router();
 
-router.route("/").get(fetchAllEmployees);
-router.route("/").post(addSingleEmployee);
+router.route("/").get(fetchAllEmployees).post(addSingleEmployee);
 router.route("/bulk").post(addMultipleEmployees);
+router.route("/:id").patch(updateEmployee);
 
 export default router;
